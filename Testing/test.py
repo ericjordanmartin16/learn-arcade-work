843c39e5295e189631ea31f45b057aa0b98232f2
Eric Martin <eric.martin@my.simpson.edu>
2021-09-02 15:00:49 -0500

    Work on testing.

---
+++ Testing/test.py
@@ -0,0 +1,10 @@
+print("Most exciting class ever!!!!")
+print("This is a second line of text")
+print("Professor Craven said \"Hi\"!")
+print("Professor Craven said \"Hi\"! \\")
+print("Professor Craven said \n\"Hi\"! \\")
+print("""Holy cow!
+The professor
+has
+gone
+crazy!""")
